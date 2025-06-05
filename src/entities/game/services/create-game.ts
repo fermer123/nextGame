13f9ce166acad5127dbd88ab589f5f09613502cc
fermer123 <fermer_525@mail.ru>
2025-06-06 02:51:19 +0500

    153

--- src/entities/game/services/create-game.ts
+++ src/entities/game/services/create-game.ts
@@ -1,13 +1,28 @@
 import cuid from 'cuid';
-import {TGameIdle, TPlayer} from '../domain';
+import {TPlayer} from '../domain';
 import {gameRepository} from '../repositories/game';
+import {left} from '@/shared/lib/either';
 
-export async function createGame(player: TPlayer): Promise<TGameIdle[]> {
-  const games = await gameRepository.gameList({
+export async function createGame(player: TPlayer) {
+  const playerGames = await gameRepository.gameList({
     status: 'idle',
     id: cuid(),
     players: {some: {id: player?.id}},
   });
+  const isGameInIdleStatus = playerGames.some(
+    (game) => game.status === 'idle' && game.creator.id === player.id,
+  );
 
-  return games as TGameIdle[];
+  if (isGameInIdleStatus) {
+    return left('can-create-only-one-game' as const);
+  }
+
+  const createdGame = await gameRepository.createGame({
+    id: cuid(),
+    creator: player,
+    status: 'idle',
+    field: Array(9).fill(null),
+  });
+
+  return createdGame;
 }
